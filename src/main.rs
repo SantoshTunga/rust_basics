ffc775c758704050cbdea6031a14e5b27d5558e4
Santosh Tunga <santosh2tunga@gmail.com>
2026-05-23 21:46:34 +0530

    rust basics

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+mod utils;
+
+use crate::utils::helper_methods::*;
+
+fn main() {
+    let mut name = "Santosh";
+    println!("Hello, world! {} ", name);
+    name = "Student";
+    println!("Hello, world! {} ", name);
+    let result =  add(1, 2);
+    println!("Hello, world! {} ", result);
+}
---
+++ src/utils/helper_methods.rs
@@ -0,0 +1,4 @@
+
+pub fn add(x:i8, y:i8) -> i8 {
+    return x + y;
+}
